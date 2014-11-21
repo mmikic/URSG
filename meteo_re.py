f5c87d422a0584d794fbb8a32e589406125189ec
mmikic <mmikic@mario-mikics-macbook-pro-2.local>
2014-11-21 20:41:15 +0100

    Prva verzija koja radi

--- meteo_re.py
+++ meteo_re.py
@@ -2,38 +2,57 @@
 import urllib2 
 import re
 
-def dohvatiPodatke():
+
+
+
+def dohvatiPodatke(izvori):
     
-    izvori = ['http://vrijeme.hr/aktpod.php?id=hrvatska_n']
-
-
-def dohvatiSadrzaj(adrese):
-
-    mjerenja = []
-
-    for adresa in adrese:
+    if type(izvori) is not list:
         
-        mjerenja.append(sortirajSadrzaj(urllib2.urlopen(adresa).read().decode("utf8")))
+        return False;
     
-    return mjerenja
+    rezultati = []
+    
+    for izvor in izvori:
+        
+        rezultati.append(dohvatiSadrzaj(izvor))
+
+    
+    if len(rezultati) > 1:
+        
+        return rezultati
+        
+    else:
+        
+        return rezultati[0]
+    
+
+def dohvatiSadrzaj(adresa):
+
+    return sortirajSadrzaj(urllib2.urlopen(adresa).read().decode("utf8"))
     
     
 def sortirajSadrzaj(datoteka):
     
     rezultati = dict()
     
-    postaje = re.findall(r'<tr bgcolor="(.*?)" align="center">(.*?)</tr>', datoteka, re.M|re.I|re.S)    
-    for postaja in dokument[1:]: 
+    postaje = re.findall(r'<tr (.*?) align="center">(.*?)</tr>', datoteka, re.M|re.I|re.S)
+      
+    for postaja in postaje[1:]: 
         
         naziv_postaje = re.findall(r'<td align="left">&nbsp;(.*?)</td>', postaja[1].strip(), re.M|re.I|re.S) # u [0]
         podaci = re.findall(r'<td>(.*?)</td>', postaja[1].strip(), re.M|re.I|re.S)
         
         # smjer vjetra, brzina vjetra, temperatura, relativna vlaznost, tlak zraka, tendencija tlaka, vrijeme
-        rezultati[naziv_postaje[0]] = {'smjer_vjetra': podaci[0], 'brzina_vjetra': podaci[1], 'temperatura_zraka': podaci[2],'relativna_vlaznost': podaci[3], 'tlak_zraka': podaci[4], 'tendencija_tlaka': podaci[5] }
+        rezultati[naziv_postaje[0]] = {'smjer_vjetra': podaci[0], 'brzina_vjetra': podaci[1], 'temperatura_zraka': podaci[2],'relativna_vlaznost': podaci[3], 'tlak_zraka': podaci[4], 'tendencija_tlaka': (re.findall(r'<font (.*?)>(.*?)</font>', podaci[5], re.M|re.I|re.S)[0][1]), 'vrijeme': podaci[6] }
         
     return rezultati
+
+
+# temperatura zraka u Senju u 06h, 08h i 12h
+for podatak in dohvatiPodatke(['http://vrijeme.hr/aktpod.php?id=hrvatska_n&param=06', 'http://vrijeme.hr/aktpod.php?id=hrvatska_n&param=08', 'http://vrijeme.hr/aktpod.php?id=hrvatska_n&param=12']):
     
-
-podaci = dohvatiPodatke()
-
-print podaci[0]['Zagreb-aer']
+    print podatak['Senj']['temperatura_zraka']
+    
+#temperatura zraka u Zagrebu sad
+print dohvatiPodatke(['http://vrijeme.hr/aktpod.php?id=hrvatska_n'])['Zagreb-aer']['temperatura_zraka']
